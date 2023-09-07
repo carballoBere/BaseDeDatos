44b9097f333d570c92db30771fb9a455185f57dd
Adriana Muruchi <carballoadriana2856@gmail.com>
2023-09-07 11:06:11 -0300

    ejercicio update

---
+++ Bmodificacion.sql/update.sql
@@ -0,0 +1,8 @@
+-- user
+UPDATE user SET username = "belen",password = "linda123"
+WHERE  username like "%adriana%"
+
+-- post
+UPDATE post SET author_id = 1, title = "holis123"
+	WHERE  author_id  = 1
+
