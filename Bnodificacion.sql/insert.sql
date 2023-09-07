83a15e32ce3f391d7b9f4727f9dc9394101176de
Adriana Muruchi <carballoadriana2856@gmail.com>
2023-09-07 10:48:39 -0300

    corregi y complete insert

--- Bnodificacion.sql/insert.sql
+++ Bnodificacion.sql/insert.sql
@@ -1,4 +1,13 @@
+-- user
 INSERT INTO user(username,password)
 values ("adriana","vele1994"),
 		("berenice","123456"),
 		("verito","chicagoo");
+    
+-- post
+INSERT INTO post(author_id,title,body)
+values (1,"pelis","terror"),
+		(2,"musica","rock nacional"),
+		(3,"series","drama");
+       
+       
