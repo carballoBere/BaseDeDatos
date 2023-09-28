f0d527d79788beabfc40b85ddb50b11e797b0662
adriana muruchi <carballoadriana2856@gmail.com>
2023-09-28 11:27:58 -0300

    insert modificado

--- Bmpostgres.sql/minsert.sql
+++ Bmpostgres.sql/minsert.sql
@@ -1,14 +1,12 @@
--- user
+--user
 INSERT INTO user(username,password)
 values ("adriana","vele1994"),
 		("berenice","123456"),
 		("verito","chicagoo"),
 		('Belen', 'AMO A SANTI');
-    
+
 -- post
-INSERT INTO post(author_id,title,body)
-values (1,"pelis","terror"),
-		(2,"musica","rock nacional"),
-		(3,"series","drama");
-       
-       
+INSERT INTO "post"(author_id,title,body)
+values (1,'pelis','terror'),
+		(2,'musica','rock nacional'),
+		(3,'series','drama');       
